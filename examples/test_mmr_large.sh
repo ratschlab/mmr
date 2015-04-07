ee6d65597f05d63f5352ceceeb88b8cd7a64d755
Andre Kahles <akahles@cbio.mskcc.org>
2015-04-07 09:18:36 -0400

    Improved correctness test for example data
    It turned out that the checksum of the result bam file was dependent on
    the compression and the samtools version used to create the bam file. To
    be agnostic with respect these factors, we now compute the checksum on
    the uncompressed result files.

--- examples/test_mmr_large.sh
+++ examples/test_mmr_large.sh
@@ -8,5 +8,8 @@ outfile=${testfile%bam}mmr.bam
 echo -e "\nRunning: ../mmr -F 0 -p -b -v -o $outfile $testfile \n"
 ../mmr -F 0 -p -b -v -o $outfile $testfile
 
+mkfifo example_file_large.ID_sorted.mmr.sam
+samtools view example_file_large.ID_sorted.mmr.bam > example_file_large.ID_sorted.mmr.sam &
 md5sum -c --quiet ${outfile}.md5 && echo -e "\nTest successfully passed" || echo -e "\nTest failed"
+rm example_file_large.ID_sorted.mmr.sam
     
